c58eaffd03ad86670e1138bf4c776726d000ea0d
Sapana2402 <sapna.prajapati2001@gmail.com>
2025-06-17 18:50:39 +0530

    Select size UI changes

---
+++ stylehut/Sources/Feature/ProductDescription/MoreColors/MoreColorsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MoreColorsCollectionViewCell.swift
+//  stylehut
+//
+//  Created by MACM26 on 17/06/25.
+//
+
+import UIKit
+
+class MoreColorsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ stylehut/Sources/Feature/ProductDescription/SelectSize/SelectSizeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SelectSizeCollectionViewCell.swift
+//  stylehut
+//
+//  Created by MACM26 on 17/06/25.
+//
+
+import UIKit
+
+class SelectSizeCollectionViewCell: UICollectionViewCell {
+    
+}
